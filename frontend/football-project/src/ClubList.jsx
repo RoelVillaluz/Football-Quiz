2ee0148230ebf38d8bb1558bd27b353899986260
RoelVillaluz <roelo.villaluz@gmail.com>
2025-01-21 16:50:20 +0800

    added club list page

--- frontend/football-project/src/ClubList.jsx
+++ frontend/football-project/src/ClubList.jsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react"
+import { Link } from "react-router-dom";
 
 function ClubList() {
 
@@ -24,19 +25,44 @@ function ClubList() {
         fetchClubs()
         document.title = 'Club List'
     }, [])
+
+    const groupedItems = clubs.reduce((acc, item) => {
+        const firstLetter = item.name[0].toUpperCase();
+        if (!acc[firstLetter]) {
+            acc[firstLetter] = [];
+        }
+        acc[firstLetter].push(item);
+        return acc;
+    }, {});
+
+    const sortedLetters = Object.keys(groupedItems).sort()
     
-    // return(
-    //     <>
-    //         <ol>
-    //             <h2>Club List</h2>
-    //             {clubs.map((club) => (
-    //                 <li key={club._id}>
-    //                     <span>{club.name}</span>
-    //                 </li>
-    //             ))}
-    //         </ol>
-    //     </>
-    // )
+    return (
+        <>
+            <section className="list-container">
+                <header>
+                    <h1>Club List</h1>
+                </header>
+                <ul>
+                    {sortedLetters.map((letter) => (
+                        <li key={letter} className="letter-group">
+                            <h2 className="letter-item">{letter}</h2>
+                            <ul id="club-list">
+                                {groupedItems[letter]
+                                    .sort((a, b) => a.name.localeCompare(b.name))
+                                    .map((club) => (
+                                        <li key={club._id}>
+                                            <Link to={`/clubs/${club._id}/${club.name}`}>{club.name}</Link>
+                                        </li>
+                                    ))
+                                }
+                            </ul>
+                        </li>
+                    ))}
+                </ul>
+            </section>
+        </>
+    )
 }
 
 export default ClubList
