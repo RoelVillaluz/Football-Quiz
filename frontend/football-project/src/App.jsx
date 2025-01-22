14ddebd5f8a42f925edc8700fecd29d08e5baffb
RoelVillaluz <roelo.villaluz@gmail.com>
2025-01-22 08:35:39 +0800

    added comments for routes for better clarity

--- frontend/football-project/src/App.jsx
+++ frontend/football-project/src/App.jsx
@@ -4,8 +4,10 @@ import ClubDetail from "./ClubDetail"
 import CreatePlayers from "./CreatePlayers"
 import CreateClubs from "./CreateClubs"
 import Navbar from "./Navbar"
+import PlayerDetail from "./PlayerDetail"
 import GuessingGame from "./GuessingGame"
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'
+import EditCLub from "./EditClub"
 
 function App() {
 
@@ -18,12 +20,19 @@ function App() {
           <div className="content">
             <Routes>
               <Route path={"/"} element={<GuessingGame/>}/>
+
+              {/* CLUBS */}
               <Route path="/clubs" element={<ClubList/>}/>
               <Route path='/clubs/:id/:name' element={<ClubDetail/>}/>
-
-              <Route path="/players" element={<PlayerList/>}/>
-              <Route path='/create-players' element={<CreatePlayers/>}/>
+              <Route path="/:id/:name" element={<ClubDetail />} />
               <Route path='/create-clubs' element={<CreateClubs/>}/>
+              <Route path='/clubs/:id/:name/edit' element={<EditCLub/>}/>
+
+              {/* PLAYERS */}
+              <Route path="/players" element={<PlayerList/>}/>
+              <Route path="/players/:id/:name" element={<PlayerDetail/>}/>
+              <Route path='/create-players' element={<CreatePlayers/>}/>
+
 
             </Routes>
           </div>
