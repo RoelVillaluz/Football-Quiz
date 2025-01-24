1f8822c9d805e4a533c0adc55cbd707c0403ec72
RoelVillaluz <roelo.villaluz@gmail.com>
2025-01-24 17:01:55 +0800

    added navbar to admin app.jsx

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,3 +1,6 @@
+import Dashboard from "./Dashboard"
+import AdminNavbar from "./AdminNavbar"
+
 import ClubList from "./Clubs/ClubList"
 import CreateClubs from "./Clubs/CreateClubs"
 import ClubDetail from "./Clubs/ClubDetail"
@@ -15,20 +18,28 @@ function App() {
   return (
     <>
       <Router>
-        <Routes>
-          {/* CLUBS */}
-          <Route path="/clubs" element={<ClubList/>}/>
-          <Route path='/clubs/:id/:name' element={<ClubDetail/>}/>
-          <Route path="/:id/:name" element={<ClubDetail />} />
-          <Route path='/create-clubs' element={<CreateClubs/>}/>
-          <Route path='/clubs/:id/:name/edit' element={<EditCLub/>}/>
+        <AdminNavbar/>
 
-          {/* PLAYERS */}
-          <Route path="/players" element={<PlayerList/>}/>
-          <Route path="/players/:id/:name" element={<PlayerDetail/>}/>
-          <Route path='/create-players' element={<CreatePlayers/>}/>
-          <Route path='/players/:id/:name/edit' element={<EditPlayer/>}/>
-        </Routes>
+          <div className="content">
+            <Routes>
+              {/* Homepage */}
+              <Route path="/" element={<Dashboard/>}/>
+
+              {/* CLUBS */}
+              <Route path="/clubs" element={<ClubList/>}/>
+              <Route path='/clubs/:id/:name' element={<ClubDetail/>}/>
+              <Route path="/:id/:name" element={<ClubDetail />} />
+              <Route path='/create-clubs' element={<CreateClubs/>}/>
+              <Route path='/clubs/:id/:name/edit' element={<EditCLub/>}/>
+
+              {/* PLAYERS */}
+              <Route path="/players" element={<PlayerList/>}/>
+              <Route path="/players/:id/:name" element={<PlayerDetail/>}/>
+              <Route path='/create-players' element={<CreatePlayers/>}/>
+              <Route path='/players/:id/:name/edit' element={<EditPlayer/>}/>
+            </Routes>
+          </div>
+          
       </Router>
     </>
   )
