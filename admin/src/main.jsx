50108a559bbe7cd3d5d2a8fb60a3143144cd3331
RoelVillaluz <roelo.villaluz@gmail.com>
2025-01-24 17:00:02 +0800

    refactor: separated navbar css into own file for better modularity

--- admin/src/main.jsx
+++ admin/src/main.jsx
@@ -1,6 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
+import  '../../frontend/football-project/src/navbar.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
