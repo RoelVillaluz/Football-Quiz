641cdf3458f43f4efe1872eac700eb57155aa247
RoelVillaluz <roelo.villaluz@gmail.com>
2025-01-25 14:33:08 +0800

    updated app.jsx

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,49 +1,44 @@
-import Dashboard from "./Dashboard"
-import AdminNavbar from "./AdminNavbar"
+import Dashboard from "./Dashboard";
+import AdminNavbar from "./AdminNavbar";
 
-import ClubList from "./Clubs/ClubList"
-import CreateClubs from "./Clubs/CreateClubs"
-import ClubDetail from "./Clubs/ClubDetail"
-import EditCLub from "./Clubs/EditClub"
+import ClubList from "./Clubs/ClubList";
+import CreateClubs from "./Clubs/CreateClubs";
+import ClubDetail from "./Clubs/ClubDetail";
+import EditCLub from "./Clubs/EditClub";
 
-import PlayerList from "./Players/PlayerList"
-import CreatePlayers from "./Players/CreatePlayers"
-import PlayerDetail from "./Players/PlayerDetail"
-import EditPlayer from "./Players/EditPlayer"
+import PlayerList from "./Players/PlayerList";
+import CreatePlayers from "./Players/CreatePlayers";
+import PlayerDetail from "./Players/PlayerDetail";
+import EditPlayer from "./Players/EditPlayer";
 
-import {BrowserRouter as Router, Routes, Route, Link} from "react-router-dom" 
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
-
   return (
     <>
       <Router>
-        <AdminNavbar/>
+        <AdminNavbar />
+        <div className="content">
+          <Routes>
+            {/* Homepage */}
+            <Route path="/" element={<Dashboard />} />
 
-          <div className="content">
-            <Routes>
-              {/* Homepage */}
-              <Route path="/" element={<Dashboard/>}/>
+            {/* CLUBS */}
+            <Route path="/clubs" element={<ClubList />} />
+            <Route path="/clubs/:id/:name" element={<ClubDetail />} />
+            <Route path="/create-clubs" element={<CreateClubs />} />
+            <Route path="/clubs/:id/:name/edit" element={<EditCLub />} />
 
-              {/* CLUBS */}
-              <Route path="/clubs" element={<ClubList/>}/>
-              <Route path='/clubs/:id/:name' element={<ClubDetail/>}/>
-              <Route path="/:id/:name" element={<ClubDetail />} />
-              <Route path='/create-clubs' element={<CreateClubs/>}/>
-              <Route path='/clubs/:id/:name/edit' element={<EditCLub/>}/>
-
-              {/* PLAYERS */}
-              <Route path="/players" element={<PlayerList/>}/>
-              <Route path="/players/:id/:name" element={<PlayerDetail/>}/>
-              <Route path='/create-players' element={<CreatePlayers/>}/>
-              <Route path='/players/:id/:name/edit' element={<EditPlayer/>}/>
-            </Routes>
-          </div>
-          
+            {/* PLAYERS */}
+            <Route path="/players" element={<PlayerList />} />
+            <Route path="/players/:id/:name" element={<PlayerDetail />} />
+            <Route path="/create-players" element={<CreatePlayers />} />
+            <Route path="/players/:id/:name/edit" element={<EditPlayer />} />
+          </Routes>
+        </div>
       </Router>
     </>
-  )
-
-
+  );
 }
-export default App
+
+export default App;
