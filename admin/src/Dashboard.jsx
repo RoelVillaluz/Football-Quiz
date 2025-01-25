17b7352d141f0f58dbf28e48c0a06754bb2e09ae
RoelVillaluz <roelo.villaluz@gmail.com>
2025-01-25 16:32:32 +0800

    added stat cards for dashboard

--- admin/src/Dashboard.jsx
+++ admin/src/Dashboard.jsx
@@ -1,4 +1,5 @@
-import { useEffect } from "react"
+import { useEffect, useState } from "react"
+import { Link } from "react-router-dom";
 
 function Dashboard() {
     const [players, setPlayers] = useState([]);
@@ -28,9 +29,44 @@ function Dashboard() {
         }
         fetchPlayers();
     }, [])
+    
     return (
         <>
-
+            <main className="dashboard">
+                <header>
+                    <h1>Dashboard</h1>
+                    <p>Welcome back! Monitor key metrics and manage your application efficiently.</p>
+                </header>
+                <section className="stats">
+                    <div className="stat-card" id="player-stat-card">
+                        <div className="wrapper">
+                            <h3>Players</h3>
+                            <Link to={'/players'}>
+                                <i class="fa-solid fa-arrow-right" id="arrow-link"></i>
+                            </Link>
+                        </div>
+                        <div className="wrapper" style={{ alignItems: 'start', justifyContent: "start", gap: '0.5rem' }}>
+                            <i class="fa-solid fa-user stat-icon"></i>
+                            <span className="count">{players.length}</span>
+                        </div>
+                        <span className="update"></span>
+                    </div>
+                    
+                    
+                    <div className="stat-card" id="club-stat-card">
+                        <div className="wrapper">
+                            <h3>Clubs</h3>
+                            <Link to={'/clubs'}>
+                                <i class="fa-solid fa-arrow-right" id="arrow-link"></i>
+                            </Link>
+                        </div>
+                        <div className="wrapper" style={{ alignItems: 'start', justifyContent: "start", gap: '0.5rem' }}>
+                            <i class="fa-solid fa-shield stat-icon"></i>
+                            <span className="count">0</span>
+                        </div>
+                    </div>
+                </section>
+            </main>
         </>
     )
 }
