338534444c4f9463e878b0683f45e50c27abfab5
RoelVillaluz <roelo.villaluz@gmail.com>
2025-01-25 16:32:21 +0800

    added useeffect hook to fetch players for dashboard

--- admin/src/Dashboard.jsx
+++ admin/src/Dashboard.jsx
@@ -1,10 +1,33 @@
 import { useEffect } from "react"
 
 function Dashboard() {
+    const [players, setPlayers] = useState([]);
+    const [error, setError] = useState(null);
+    const [isLoading, setIsLoading] = useState(true);
+
     useEffect(() => {
         document.title = 'Admin Dashboard'
     }, [])
 
+    useEffect(() => {
+        const fetchPlayers = async () => {
+            try {
+                const response = await fetch('http://localhost:5000/api/players')
+                if (!response.ok) {
+                    throw new Error('Erorr', response.status)
+                }
+                const responseData = await response.json()
+
+                console.log(responseData.data)
+                setPlayers(responseData.data)
+            } catch (error) {
+                console.error('Error', error)
+            } finally {
+                setIsLoading(false)
+            }
+        }
+        fetchPlayers();
+    }, [])
     return (
         <>
 
