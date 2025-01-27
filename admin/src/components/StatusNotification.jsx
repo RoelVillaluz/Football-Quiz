90cad2784bb6c9fd487f8a23311e44f8c8556db7
RoelVillaluz <roelo.villaluz@gmail.com>
2025-01-27 08:30:17 +0800

    added timeout to notification so it disappears after few seconds

--- admin/src/components/StatusNotification.jsx
+++ admin/src/components/StatusNotification.jsx
@@ -1,10 +1,18 @@
-function StatusNotification ({ type, object, action, visible }) {
-    // type: type of model (player, club)
-    // name: player or club object
-    // action: post, edit, delete
+import { useEffect, useState } from "react"
+
+function StatusNotification ({ type, object, action, visible, duration=3000 }) {
+    const [isVisible, setIsVisible] = useState(false);
+
+    useEffect(() => {
+        if (visible) {
+            setIsVisible(true)
+            const timer = setTimeout(() => setIsVisible(false), duration)
+            return () => clearTimeout(timer)
+        }
+    }, [visible, duration])
 
     return (
-        <div className={`notification ${visible ? 'show' : 'hidden' }`}>
+        <div className={`notification ${isVisible ? 'show' : 'hidden' }`}>
             <img src={`http://localhost:5000${object.image}`} alt="" />
             <div>
                 <h3>{ object.name }</h3>
