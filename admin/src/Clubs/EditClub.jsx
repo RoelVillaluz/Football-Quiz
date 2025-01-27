1649d8f6dcbb45b7ab0cc8f38eea57b75e34adb4
RoelVillaluz <roelo.villaluz@gmail.com>
2025-01-27 13:53:40 +0800

    added EditForm component to Edit club page

--- admin/src/Clubs/EditClub.jsx
+++ admin/src/Clubs/EditClub.jsx
@@ -1,34 +1,41 @@
-import { useEffect, useState } from "react"
-import { Link, useParams } from "react-router-dom"
+import { useEffect } from "react";
+import { useParams } from "react-router-dom";
+import EditForm from "../components/EditForm";
 import { useData } from "../DataProvider";
 
 function EditClub() {
     const { id } = useParams();  
-    const { club, fetchClub } = useData();
+    const { club, fetchClub, isLoading } = useData();
 
     useEffect(() => {
-        fetchClub(id)
-    }, [id, fetchClub])
+        fetchClub(id);
+    }, [id, fetchClub]);
 
     return (
-        <>
-            <section className="edit-section">
-                    <div className="edit-form-container">
-                        <header>
-                            <div className="wrapper">
-                                <figure className="player-icon">
-                                    <img src={`http://localhost:5000${club.image}`} alt="" />
-                                </figure>
-                                <div>
-                                    <h1>Edit Club</h1>
-                                    <span className="name">{club.name}</span>
-                                </div>
-                            </div>
-                        </header>
-                        <EditForm model="clubs"/>
+        <section className="edit-section">
+            <div className="edit-form-container">
+                <header>
+                    <div className="wrapper">
+                        <figure className="player-icon">
+                            {isLoading || !club ? (
+                                <p>Loading...</p>
+                            ) : (
+                                <img src={`http://localhost:5000${club.image}`} alt="" />
+                            )}
+                        </figure>
+                        <div>
+                            <h1>Edit Club</h1>
+                            {isLoading || !club ? (
+                                <p>Loading...</p>
+                            ) : (
+                                <span className="name">{club.name}</span>
+                            )}
+                        </div>
                     </div>
-            </section>
-        </>
+                </header>
+                <EditForm model="clubs" objectToEdit={club} />
+            </div>
+        </section>
     );
 }
 
