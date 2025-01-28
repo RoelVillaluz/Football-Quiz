1af8e686c5393e52f12b5339de0cc48fa14a5216
RoelVillaluz <roelo.villaluz@gmail.com>
2025-01-28 09:09:36 +0800

    refactor: used editForm component for edit player

--- admin/src/Players/EditPlayer.jsx
+++ admin/src/Players/EditPlayer.jsx
@@ -1,41 +1,17 @@
 import { useEffect, useState } from "react"
-import axios from "axios"
 import { Link, useParams } from "react-router-dom"
+import axios from "axios";
 import StatusNotification from "../components/StatusNotification";
+import EditForm from "../components/EditForm";
 import Select from "react-select";
+import { useData } from "../DataProvider";
 
 function EditPlayer() {
     const { id } = useParams();
-    const [player, setPlayer] = useState({ name: "", clubs: [], image: "" })
-    const [clubs, setClubs] = useState([])
-    const [error, setError] = useState(null)
-    const [success, setSuccess] = useState(false);
-    const [successMessage, setSuccessMessage] = useState("");
-    const [formData, setFormData] = useState({ name: "", clubs:[], image: "" })
-    const [imagePreview, setImagePreview] = useState("")
+    const { player, fetchPlayer, clubs, setClubs, isLoading, success } = useData();
 
     useEffect(() => {
-        const fetchPlayer = async () => {
-            try {
-                const response = await axios.get(`http://localhost:5000/api/players/${id}`)
-
-                const playerData = response.data.data || { name: "", clubs: [], image: "" }
-                setPlayer(playerData)
-    
-                // prefill form data with player info
-                setFormData({ 
-                    name: playerData.name, 
-                    clubs: playerData.clubs.map((club) => ({
-                        value: club._id,
-                        label: club.name,
-                    })), 
-                    image: playerData.image 
-                })
-            } catch (error) {
-                console.error('Error', error)
-            }
-        }
-        fetchPlayer()
+        fetchPlayer(id)
     }, [id])
 
     useEffect(() => {
@@ -53,69 +29,6 @@ function EditPlayer() {
         }
         fetchClubs();
     }, []);
-    
-    useEffect(() => {
-        document.title = `Edit: ${player.name}`
-    }, [player])
-
-    const handleInputChange = (e) => {
-        const { name, value, files } = e.target;
-        
-        if (name === "image" && files.length > 0) {
-            const file = files[0];
-            setFormData((prev) => ({ ...prev, image: file }))
-            setImagePreview(URL.createObjectURL(file))
-        } else {
-            setFormData((prev) => ({ ...prev, [name]: value }))
-        }
-    };
-
-    // Handle React-Select change for clubs
-    const handleClubsChange = (selectedOptions) => {
-        setFormData((prev) => ({
-            ...prev,
-            clubs: selectedOptions, // Update clubs with selected options
-        }));
-    };
-
-    const handleFormSubmit = async (e) => {
-        e.preventDefault();
-    
-        const formDataToSend = new FormData();
-        formDataToSend.append("name", formData.name);
-        formDataToSend.append("clubs", JSON.stringify(formData.clubs.map((club) => club.value)));
-    
-        if (formData.image) {
-            formDataToSend.append("image", formData.image);
-        }
-    
-        try {
-            const response = await axios.patch(`http://localhost:5000/api/players/${id}`, formDataToSend, {
-                headers: { "Content-Type": "multipart/form-data" },
-            });
-    
-            setSuccess(true);
-            setSuccessMessage(`Player: ${formData.name} updated successfully`);
-            setError(null);
-    
-            // Refetch player data after update
-            const updatedPlayer = response.data.data;
-            setPlayer(updatedPlayer);
-            setFormData({ 
-                name: updatedPlayer.name, 
-                image: updatedPlayer.image, 
-                clubs: updatedPlayer.clubs.map((club) => ({
-                    value: club._id,
-                    label: club.name
-                }))
-            });
-        } catch (error) {
-            console.error('Error updating player', error);
-            setError(error.response?.data?.message || "Something went wrong");
-            setSuccess(false);
-        }
-    };
-    
 
     return (
         <>
@@ -124,64 +37,23 @@ function EditPlayer() {
                     <header>
                         <div className="wrapper">
                             <figure className="player-icon">
-                                <img src={`http://localhost:5000${player.image}`} alt="" />
+                                {isLoading || !player ? (
+                                    <p>Loading...</p>
+                                ) : (
+                                    <img src={`http://localhost:5000${player.image}`} alt="" />
+                                )}
                             </figure>
                             <div>
                                 <h1>Edit Player</h1>
-                                <span className="name">{player.name}</span>
+                                {isLoading || !player ? (
+                                    <p>Loading...</p>
+                                ) : (
+                                    <span className="name">{player.name}</span>
+                                )}
                             </div>
                         </div>
                     </header>
-                    <form action="" onSubmit={handleFormSubmit}>
-
-                        <div className="edit-image-container">
-
-                            <div className="image-box" onClick={() => document.getElementById('file-input').click()}>
-                                {imagePreview ? (
-                                    <>
-                                        <img src={imagePreview} alt=""className="image-preview" />
-                                        <span className="image-name">{formData.image?.name}</span >
-                                    </>
-                                ) : (
-                                    <>
-                                        <i className="fa-solid fa-image"></i>
-                                        <span>Drag file here to upload.</span>
-                                        <p>Or you can select file by <span className="highlight">clicking here</span></p>
-                                    </>
-                                )}
-                                <input type="file" name="image" id="file-input" onChange={handleInputChange}/>
-                            </div>
-                            
-                        </div>
-
-                        <div className="form-group">
-                            <label>Name:</label>
-                            <input type="text" 
-                                   name="name" 
-                                   id="name-input" onChange={handleInputChange} 
-                                   placeholder="Name" 
-                                   value={formData.name}/>
-                        </div>
-
-                        <div className="form-group">
-                            <label>Clubs:</label>
-                            <Select
-                                className="select-form"
-                                name="clubs"
-                                value={formData.clubs}
-                                options={clubs} // Set options for React-Select
-                                onChange={handleClubsChange}
-                                isMulti // Allow multiple selections
-                                placeholder="Select clubs..."
-                            />
-                        </div>
-
-                        <div className="actions">
-                            <button className="reset">Reset</button>
-                            <button className="submit">Submit</button>
-                        </div>
-
-                    </form>
+                    <EditForm model="players" objectToEdit={player}/>
                 </div>
             </section>
             {success && (
